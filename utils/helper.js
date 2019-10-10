cbb7034b17291d4229e6d7dae4e6701e39ea0b9f
Sulaiman Agbenla <sulaimanagbenla@Sulaimans-MacBook-Pro.local>
2019-10-10 16:30:25 +0100

    mocha tests added

---
+++ utils/helper.js
@@ -0,0 +1,43 @@
+const addMultipleNumbers = (numberArray) => {
+    let total = 0;
+
+    if (numberArray.length === 0) {
+        return false;
+    }
+
+    numberArray.forEach(number => {
+        total += number;
+    });
+
+    return total;
+}
+
+const isAuthorizedPromise = (userList, user) => {
+
+    const checkForUserAfterTimeout = () => { 
+        
+        return new Promise((res) => {
+            const p = new Promise((resolve) => {
+
+                setTimeout(() => { resolve(userList.indexOf(user) >= 0); }, 10);
+                
+            })
+            
+    
+            p.then((value) => {
+                res(value)
+            })
+        })
+        
+    }
+ 
+    
+    return new Promise((resolve) => {
+        resolve(checkForUserAfterTimeout());
+    });
+  }
+
+module.exports = {
+    addMultipleNumbers,
+    isAuthorizedPromise
+}
